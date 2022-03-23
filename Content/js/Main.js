da973dd7d1d3d5044e112178d2ab272e93501b98
SaraKhakifirooz <88978683+SaraKhakifirooz@users.noreply.github.com>
2022-03-23 11:26:27 +0430

    del swiper

--- Content/js/Main.js
+++ Content/js/Main.js
@@ -88,20 +88,6 @@ jQuery('document').ready(function($) {
     });
 });
 
-var swiper = new Swiper('.swiper-tesimonial', {
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
-
 
 $('#pointer_index a').click(function(e) {
     e.preventDefault();
